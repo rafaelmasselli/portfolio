e86690ed6da5bbbca8f14a6fd34d8caf28ac11fe
rafaelmasselli <rafaelmasselli0@gmail.com>
2023-12-12 14:13:35 -0300

    feat: create responsive navbar

--- src/components/shared/navbar/index.tsx
+++ src/components/shared/navbar/index.tsx
@@ -1,28 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
 const linkStyle = { color: "#dedede", textDecoration: "none" };
 
 export function Navbar() {
+  const [responsive, setResponsive] = useState(false);
+
+  function ResponsiveBtn() {
+    setResponsive(prevState => !prevState);
+  }
+
   return (
-    <header className="container navbar">
-      <nav>
-        <p>Rafael Masselli</p>
-      </nav>
-      <nav>
-        <ul className="container">
-          <Link to="/" style={linkStyle}>
-            <li>Home</li>
-          </Link>
-          <Link to="/" style={linkStyle}>
-            <li>Work</li>
-          </Link>
-          <Link to="/" style={linkStyle}>
-            <li>Contact</li>
-          </Link>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header className="container navbar">
+        <nav>
+          <p>Rafael Masselli</p>
+        </nav>
+        <nav>
+          <ul className="container responsive__navigation">
+            <Link to="/" style={linkStyle}>
+              <li>Home</li>
+            </Link>
+            <Link to="/" style={linkStyle}>
+              <li>Work</li>
+            </Link>
+            <Link to="/" style={linkStyle}>
+              <li>Contact</li>
+            </Link>
+          </ul>
+          <label className={`responsive__line__navbar ${responsive ? "active" : ""}`} onClick={ResponsiveBtn}>
+            <div className="line line1"></div>
+            <div className="line line2"></div>
+            <div className="line line3"></div>
+          </label>
+        </nav>
+      </header>
+      <ul className={`container responsive__navbar ${responsive ? "visible" : ""}`}>
+        <Link to="/" style={linkStyle}>
+          <li>Home</li>
+        </Link>
+        <Link to="/" style={linkStyle}>
+          <li>Work</li>
+        </Link>
+        <Link to="/" style={linkStyle}>
+          <li>Contact</li>
+        </Link>
+      </ul>
+    </>
   );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import "./style.css";
 
-
 export function Home() {
   return (
     <div className="container">
-
+      <main>
+        <div>
+          <div>
+            <h2></h2>
+            <h5></h5>
+            <button></button>
+          </div>
+          <div>
+            {/* <img src="https://github.com/rafaelmasselli.png" alt="" /> */}
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
