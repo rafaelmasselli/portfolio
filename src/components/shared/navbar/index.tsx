ccb9491676e7654d0552ce0e697bf920d7455863
rafaelmasselli <rafaelmasselli0@gmail.com>
2024-01-04 23:10:55 -0300

    feat:  create navbar with tailwind

--- src/components/shared/navbar/index.tsx
+++ src/components/shared/navbar/index.tsx
@@ -1,52 +1,126 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./style.css";
-
-const linkStyle = { color: "#dedede", textDecoration: "none" };
 
 export function Navbar() {
-  const [responsive, setResponsive] = useState(false);
-
-  function ResponsiveBtn() {
-    setResponsive(prevState => !prevState);
-  }
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <>
-      <header className="container navbar">
-        <nav>
-          <p>Rafael Masselli</p>
-        </nav>
-        <nav>
-          <ul className="container responsive__navigation">
-            <Link to="/" style={linkStyle}>
-              <li>Home</li>
-            </Link>
-            <Link to="/" style={linkStyle}>
-              <li>Work</li>
-            </Link>
-            <Link to="/" style={linkStyle}>
-              <li>Contact</li>
-            </Link>
-          </ul>
-          <label className={`responsive__line__navbar ${responsive ? "active" : ""}`} onClick={ResponsiveBtn}>
-            <div className="line line1"></div>
-            <div className="line line2"></div>
-            <div className="line line3"></div>
-          </label>
-        </nav>
-      </header>
-      <ul className={`container responsive__navbar ${responsive ? "visible" : ""}`}>
-        <Link to="/" style={linkStyle}>
-          <li>Home</li>
-        </Link>
-        <Link to="/" style={linkStyle}>
-          <li>Work</li>
-        </Link>
-        <Link to="/" style={linkStyle}>
-          <li>Contact</li>
-        </Link>
-      </ul>
-    </>
+    <nav className="bg-neutral-800 shadow-lg">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center justify-between w-full">
+            <div className="flex-shrink-0">
+              <span className="text-white text-lg font-bold">
+                Rafael Masselli
+              </span>
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <Link
+                  to="#"
+                  className="text-gray-300 hover:bg-gray-500 hover:text-white px-3 py-2 rounded-md text-sm font-semibold"
+                >
+                  Inicio
+                </Link>
+                <Link
+                  to="#"
+                  className="text-gray-300 hover:bg-gray-500 hover:text-white px-3 py-2 rounded-md text-sm font-semibold"
+                >
+                  Contato
+                </Link>
+                <Link
+                  to="#"
+                  className="text-gray-300 hover:bg-gray-500 hover:text-white px-3 py-2 rounded-md text-sm font-semibold"
+                >
+                  GBA
+                </Link>
+                <Link
+                  to="#"
+                  className="text-gray-300 hover:bg-gray-500 hover:text-white px-3 py-2 rounded-md text-sm font-semibold"
+                >
+                  Projetos
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+              className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-500 focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {!isOpen ? (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile menu */}
+      {isOpen && (
+        <div className="md:hidden" id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700  hover:text-white block px-3 py-2 rounded-md text-base font-semibold"
+            >
+              Inicio
+            </a>
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-semibold"
+            >
+              Contato
+            </a>
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-semibold"
+            >
+              GBA
+            </a>
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-semibold"
+            >
+              Projetos
+            </a>
+          </div>
+        </div>
+      )}
+    </nav>
   );
 }
+
+export default Navbar;
