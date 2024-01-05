a2aca0ec7c0789b6fae97c7d9b886244776bfaea
rafaelmasselli <rafaelmasselli0@gmail.com>
2024-01-04 22:39:31 -0300

    chore: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        "gray-70": "rgba(50, 50, 50, 0.70);",
+      },
+    },
+  },
+  plugins: [],
+};
