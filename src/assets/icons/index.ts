73822f63a486d08939d79299be5701f20491b371
rafaelmasselli <rafaelmasselli0@gmail.com>
2024-01-09 20:55:26 -0300

    chore: technology icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,23 @@
+import NodeJs from "./icons8-nó-js.svg";
+import ReactJs from "./icons8-reagir.svg";
+import JavaScript from "./icons8-javascript.svg";
+import TypeScript from "./icons8-texto-datilografado.svg";
+import Docker from "./icons8-docker.svg";
+import Html from "./icons8-html.svg";
+import Css from "./icons8-css.svg";
+import Sass from "./icons8-atrevimento.svg";
+import VueJs from "./icons8-visualizar-js.svg";
+
+const icons = [
+  { name: "NodeJs", icon: NodeJs },
+  { name: "ReactJs", icon: ReactJs },
+  { name: "VueJs", icon: VueJs },
+  { name: "Docker", icon: Docker },
+  { name: "JavaScript", icon: JavaScript },
+  { name: "TypeScript", icon: TypeScript },
+  { name: "Html", icon: Html },
+  { name: "Css", icon: Css },
+  { name: "Sass", icon: Sass },
+];
+
+export { icons };
