39aec41cde4d5eb3cd3d8d5e6c6beb93625d43c4
rafaelmasselli <rafaelmasselli0@gmail.com>
2024-01-09 17:20:38 -0300

    feat: create button contact

--- src/components/structure/button/index.tsx
+++ src/components/structure/button/index.tsx
@@ -1,21 +1,29 @@
 import React from "react";
-import github from "../../../assets/icons8-github-48.png";
 
 import "./style.css";
+import { openContact } from "../../../util/contact";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  title: string;
-  showIcon: boolean;
+  link: string;
 }
 
-export function Button({ showIcon, title, ...props }: ButtonProps) {
+export function Button({ link, ...props }: ButtonProps) {
+  let title;
+  const Link = link[0].toLocaleLowerCase();
+
+  if (Link === "c") {
+    title = "Currículo";
+  } else if (Link === "g") {
+    title = "GitHub";
+  } else {
+    title = "LinkedIn";
+  }
   return (
-    <button {...props} className="btn container">
-      {showIcon && (
-        <figure>
-          <img src={github} />
-        </figure>
-      )}
+    <button
+      {...props}
+      onClick={() => openContact(link)}
+      className="bg-neutral-700 hover:bg-neutral-600 text-white font-bold py-2 px-4 rounded m-1"
+    >
       {title}
     </button>
   );
